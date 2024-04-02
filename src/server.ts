93140e0e995f2157785c5a6cd639db8a90e73d5f
mark ramy <markramyy@gmail.com>
2024-04-02 16:42:09 +0200

    Add user sign up and sign in handlers, and protect routes with authentication

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 
+import { signUp } from './handlers/user'
+import { signIn } from './handlers/user'
+
 import usersRouter from './routes/users'
 import productsRouter from './routes/products'
 import ordersRouter from './routes/orders'
@@ -7,6 +10,8 @@ import orderItemsRouter from './routes/orderItems'
 
 import morgan from 'morgan'
 import cors from 'cors'
+
+import { protect } from './modules/auth'
 
 const app = express()
 
@@ -17,10 +22,10 @@ app.use(express.json()) // allows for JSON payloads
 app.use(express.urlencoded({ extended: true })) // allows for nested objects in query strings
 
 // Custom middleware
-app.use((req, res, next) => {
-	console.log('Hello from middleware')
-	next()
-})
+// app.use((req, res, next) => {
+// 	console.log('Hello from middleware')
+// 	next()
+// })
 
 app.get('/', (req, res) => {
 	console.log('Hello from Express')
@@ -29,9 +34,13 @@ app.get('/', (req, res) => {
 })
 
 // Routes
-app.use('/api/users', usersRouter)
-app.use('/api/products', productsRouter)
-app.use('/api/orders', ordersRouter)
-app.use('/api/order-items', orderItemsRouter)
+app.post('/signup', signUp)
+app.post('/signin', signIn)
+
+app.use('/api/users', protect, usersRouter)
+app.use('/api/products', protect, productsRouter)
+app.use('/api/orders', protect, ordersRouter)
+app.use('/api/order-items', protect, orderItemsRouter)
+
 
 export default app
