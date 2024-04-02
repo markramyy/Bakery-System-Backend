f387f10d86aeaed01aab90f38cbc46bf78d7f7c1
mark ramy <markramyy@gmail.com>
2024-04-02 15:11:01 +0200

    Add cors and morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import express from 'express'
 import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
+
+// Third-party middleware
+app.use(cors()) // allows for cross-origin requests
+app.use(morgan('dev')) // logs requests to the console
+app.use(express.json()) // allows for JSON payloads
+app.use(express.urlencoded({ extended: true })) // allows for nested objects in query strings
+
+// Custom middleware
+app.use((req, res, next) => {
+	console.log('Hello from middleware')
+	next()
+})
 
 app.get('/', (req, res) => {
 	console.log('Hello from Express')
