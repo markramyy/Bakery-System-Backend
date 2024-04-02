6bdea4246aefb45b91f6b9a923eb498ccda2ce79
mark ramy <markramyy@gmail.com>
2024-04-02 16:52:55 +0200

    Refactor user sign up handler to handle existing email and username

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -4,26 +4,48 @@ import { comparePassword, createJWT, hashPassword } from "../modules/auth"
 
 
 export const signUp = async (req, res) => {
-    const { username, email, password, role } = req.body
+    const { username, email, password, role } = req.body;
 
-    const hashedPassword = await hashPassword(password)
+    try {
+        // Check if email or username already exists
+        const existingUser = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { email },
+                    { username }
+                ],
+            },
+        });
 
-    const user = await prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-            role
+        if (existingUser) {
+            let message = 'Email already exists, try another one.';
+            if (existingUser.username === username) {
+                message = 'Username already exists, try another one.';
+            }
+            res.status(409).json({ message }); // 409 Conflict
+            return;
         }
-    })
 
-    const token = createJWT(user)
-    res.status(201)
-    res.json({
-        message: 'User has been created successfully',
-        token: token
-    });
-}
+        const hashedPassword = await hashPassword(password);
+
+        const user = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+                role,
+            },
+        });
+
+        const token = createJWT(user);
+        res.status(201).json({
+            message: 'User has been created successfully',
+            token: token,
+        });
+    } catch (error) {
+        res.status(400).json({ message: 'Something went wrong', error: error.message });
+    }
+};
 
 
 export const signIn = async (req, res) => {
