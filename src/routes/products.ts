400d195c3ff2f2f77597288d398759ff0daf6fc5
mark ramy <markramyy@gmail.com>
2024-04-04 10:54:34 +0200

    Refactor product routes and functionality

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,26 +3,16 @@ import prisma from '../modules/db';
 import { Router } from 'express'
 import { handleInputError, isStaff, responseFormatter, errorHandler, validateProductId, isCreator } from '../modules/middleware';
 import { body } from 'express-validator';
+import * as productHandlers from '../handlers/products';
 
 
 const router = Router()
 
 router.use(responseFormatter);
 
-router.get('/', async (req, res) => {
-    const products = await prisma.product.findMany();
-    res.formattedJson(200, products);
-});
+router.get('/', productHandlers.listProducts);
 
-router.get('/:id', validateProductId, async (req, res) => {
-    const product = await prisma.product.findUnique({
-        where: { id: req.params.id },
-    });
-    if (!product) {
-        return res.status(404).json({ message: 'Product not found' });
-    }
-    res.formattedJson(200, product);
-});
+router.get('/:id', validateProductId, productHandlers.getProductById);
 
 router.post(
     '/',
@@ -34,20 +24,7 @@ router.post(
         body('stock').notEmpty().isInt({ min: 0 }),
         handleInputError,
     ],
-    async (req, res) => {
-        const { name, description, price, stock } = req.body;
-        const creatorId = req.user.id;
-        const newProduct = await prisma.product.create({
-            data: {
-                name,
-                description,
-                price,
-                stock,
-                creatorId,
-            },
-        });
-        res.formattedJson(201, newProduct, 'Product successfully created');
-    }
+    productHandlers.createProduct
 );
 
 router.put(
@@ -62,21 +39,10 @@ router.put(
         body('stock').optional().isInt({ min: 0 }).withMessage('Stock must be a non-negative integer'),
         handleInputError,
     ],
-    async (req, res) => {
-        const updatedProduct = await prisma.product.update({
-            where: { id: req.params.id },
-            data: req.body,
-        });
-        res.formattedJson(200, updatedProduct, 'Product successfully updated');
-    }
+    productHandlers.updateProduct
 );
 
-router.delete('/:id', validateProductId, isStaff, isCreator, async (req, res) => {
-    await prisma.product.delete({
-        where: { id: req.params.id },
-    });
-    res.formattedJson(204, null, 'Product successfully deleted');
-});
+router.delete('/:id', validateProductId, isStaff, isCreator, productHandlers.deleteProduct);
 
 router.use(errorHandler);
 
