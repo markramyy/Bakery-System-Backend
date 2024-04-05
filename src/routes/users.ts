a15fcff2029f4084492d09e88662c68ce2b12509
mark ramy <markramyy@gmail.com>
2024-04-05 11:51:05 +0200

    Refactor user routes and handlers in users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,19 +1,33 @@
 import { Router } from 'express';
+import * as userHandlers from '../handlers/user';
 
 
 const router = Router();
 
+// TO BE Removed
 router.get('/', (req, res) => {
     res.json({ message: 'Hello All Users' });
 });
 
-router.get('/:id', () => {});
+router.get('/me', (req, res) => {
+    userHandlers.me(req, res);
+});
 
-router.post('/', () => {});
+router.post('/:id/change-password', (req, res) => {
+    userHandlers.changePassword(req, res);
+});
 
-router.put('/:id', () => {});
+router.post('/:id/validate-password', (req, res) => {
+    userHandlers.validatePassword(req, res);
+});
 
-router.delete('/:id', () => {});
+router.put('/:id/update-me', (req, res) => {
+    userHandlers.updateMe(req, res);
+});
 
+// NEED ADDITIONAL LOGIC IN THE HANDLER
+router.delete('/:id', (req, res) => {
+    userHandlers.deleteUser(req, res);
+});
 
 export default router;
