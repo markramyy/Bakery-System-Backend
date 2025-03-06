53c2c4683ea061edbbbce46e9cc6b4a91c6bb576
Mark Ramy <markramyy@gmail.com>
2025-03-06 16:57:50 +0200

    feat: enhance Swagger UI integration and serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import express from 'express';
 import config from './config';
 import app from './server';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger';
+import path from 'path';
+
+// Serve static files for Swagger UI
+app.use('/api-docs', express.static(path.join(__dirname, 'node_modules/swagger-ui-dist')));
 
 // Serve Swagger documentation
 app.use('/api-docs', swaggerUi.serve);
@@ -13,6 +18,11 @@ app.get(
     swaggerUi.setup(swaggerSpec, {
         explorer: true,
         customSiteTitle: 'Bakery API Documentation',
+        customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css',
+        customJs: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-bundle.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-standalone-preset.js',
+        ],
         swaggerOptions: {
             persistAuthorization: true,
         },
