7a4520118ba34897bf97c22bdf7fb0e7da3d4f54
Mark Ramy <markramyy@gmail.com>
2025-03-06 15:38:59 +0200

    vercel

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,22 +4,25 @@ import merge from 'lodash/merge';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const stage = process.env.STAGE || 'local';
 
-let envConfig
+let envConfig;
 
 if (stage === 'production') {
-  envConfig = require('./production').default
+    envConfig = require('./production').default;
 } else if (stage === 'testing') {
-    envConfig = require('./testing').default
+    envConfig = require('./testing').default;
 } else {
-    envConfig = require('./local').default
+    envConfig = require('./local').default;
 }
 
-export default merge({
-    stage: stage,
-    env: process.env.NODE_ENV,
-    port: 3001,
-    secrets: {
-        jwt: process.env.JWT_SECRET,
-        dbUrl: process.env.DATABASE_URL,
-    }
-    }, envConfig)
+export default merge(
+    {
+        stage: stage,
+        env: process.env.NODE_ENV,
+        port: 3001,
+        secrets: {
+            jwt: process.env.JWT_SECRET,
+            dbUrl: process.env.DATABASE_URL,
+        },
+    },
+    envConfig,
+);
